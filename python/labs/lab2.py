9315c6742d7b73580c154e9f265fd4cdc9c82dbe
Lynn <lynn@MacBook-Air.localdomain>
2020-02-26 20:59:35 -0800

    first commit

--- python/labs/lab2.py
+++ python/labs/lab2.py
@@ -1,31 +1,31 @@
 #Version 1
-#  antonym = input ("Give me an antonym for data: ")
-# adjective = input ("Tell me an adjective: ")
-# buzzwood = input ("Give me a sciency buzzword: ")
-# animal = input ("A type of animal (plural): ")
-# sciency = input ("some sciency thing: ")
-# sciency2 = input ("another sciency thing: ")
-# sci_adj = input ("sciency adjective: ")
+antonym = input ("Give me an antonym for data: ")
+adjective = input ("Tell me an adjective: ")
+buzzwood = input ("Give me a sciency buzzword: ")
+animal = input ("A type of animal (plural): ")
+sciency = input ("some sciency thing: ")
+sciency2 = input ("another sciency thing: ")
+sci_adj = input ("sciency adjective: ")
 
-# print (f"{antonym} Scientist Job Description: Seeking a {adjective} engineer, able to work on {buzzwood} projects with a team of {animal}. Key responsibilities: Extract patterns from non-material. Optimize {sciency}. Transform {sciency2} into {sci_adj} material.")
+print (f"{antonym} Scientist Job Description: Seeking a {adjective} engineer, able to work on {buzzwood} projects with a team of {animal}. Key responsibilities: Extract patterns from non-material. Optimize {sciency}. Transform {sciency2} into {sci_adj} material.")
 
 
 # Version 2 
 
-# mad_lib = input("Name 3 adjectives ")
-# answer = mad_lib.split()
+mad_lib = input("Name 3 adjectives ")
+answer = mad_lib.split()
 
-# print (f"My {answer[0]} is {answer[1]} and {answer[2]}! ")
-
-
+print (f"My {answer[0]} is {answer[1]} and {answer[2]}! ")
 
 
-# import random
 
-# mad_lib = input("Enter 3 adjectives ")
-# answer = mad_lib.split(', ')
 
-# print("my", random.choice(answer), "is", random.choice(answer), "the", random.choice(answer), "!" )
+import random
+
+mad_lib = input("Enter 3 adjectives ")
+answer = mad_lib.split(', ')
+
+print("my", random.choice(answer), "is", random.choice(answer), "the", random.choice(answer), "!" )
 
 #Version 3 
 
@@ -42,12 +42,23 @@ print (f"{antonym} Scientist Job Description: Seeking a {adjective} engineer, ab
 question = input ("Do you want to hear the story again? ")
 
 
-while question == "yes":
+while(question == "yes"):
+    antonym = input ("Give me an antonym for data: ")
+    adjective = input ("Tell me an adjective: ")
+    buzzwood = input ("Give me a sciency buzzword: ")
+    animal = input ("A type of animal (plural): ")
+    sciency = input ("some sciency thing: ")
+    sciency2 = input ("another sciency thing: ")
+    sci_adj = input ("sciency adjective: ")
     print(f"{antonym} Scientist Job Description: Seeking a {adjective} engineer, able to work on {buzzwood} projects with a team of {animal}. Key responsibilities: Extract patterns from non-material. Optimize {sciency}. Transform {sciency2} into {sci_adj} material.")
-    print(question)
+    question = input ("Do you want to hear the story again? ")
     if question == "no":
         print("Goodbye! ")
-    break
+     
+    
+
+    
+
  
     
 
@@ -56,15 +67,4 @@ while question == "yes":
 
 
 
-
-# for each_item in answer:
-#     print(each_item)
-
-
-# nums = [3, 5, 2, 3, 45, 23, 42]
-
-# for each_item in nums:
-#     print(each_item)
-
-# print(nums[-2]) 
 
