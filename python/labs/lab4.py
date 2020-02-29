5f67d72a5d76623de00bc79cb4eae5907a872edf
Lynn <lynn@MacBook-Air.localdomain>
2020-02-28 20:31:36 -0800

    lab 4 upload

---
+++ python/labs/lab4.py
@@ -0,0 +1,21 @@
+"""
+Print a welcome screen to the user.
+Use the random module's random.choice() to choose a prediction.
+Prompt the user to ask the 8-ball a question "will I win the lottery tomorrow?"
+Display the result of the 8-ball.
+"""
+
+from random import choice
+
+while True:
+
+    predictions = ["it is certain", "very doubtful", "concentrate and ask again", "signs point to yes", "don't count on it."]
+
+    user_input = input("Please ask, Will I win the lottery tomorrow? ")
+
+    print(choice(predictions))
+    
+    if(input('Would you like to play again (y/n): ').lower() != 'y'):
+        print(" Goodbye! ")
+        break 
+
