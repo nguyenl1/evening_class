39671d90f6a3a13083a141037e6aae2da2abdd19
Lynn <lynn@Sheris-MacBook-Air.local>
2020-03-23 19:03:32 -0700

    lab 18 update

--- python/labs/lab18.py
+++ python/labs/lab18.py
@@ -6,81 +6,49 @@ valleys - Returns the indices of 'valleys'. A valley is a number with a higher n
 peaks_and_valleys - uses the above two functions to compile a single list of the peaks and valleys in order of appearance in the original data.
 
 # """
-# data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-# noindex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-# POI = [' ',' ',' ',' ',' ',' ','P',' ',' ','V',' ',' ',' ',' ','P',' ',' ','V',' ',' ',' ',]
+data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+noindex= [0, 1, 2, 3, 4, 5, 6, 7, 8,9, 10, 11, 12, 13,14,15,16,17,18,19,20]
 
-# def peaks():
-#     length = len(data)
-#     middle_index = length//2
+def peaks():
+    length = len(data)
+    middle_index = length//2
 
-#     first_half = data[:middle_index]
-#     second_half = data[middle_index:]
-#     # peak = first_half.index('P')
+    first_half = data[:middle_index]
+    second_half = data[middle_index:]
+    # peak = first_half.index('P')
 
-#     peak = data.index(max(first_half))
-#     peak_2 = data.index(max(second_half))
-
-    # if peak_2 == data[0] or peak_2 == data[20]:
+    peak = data.index(max(first_half))
+    peak_2 = data.index(max(second_half))
 
 
-    # print(f"The index of the peak on the left is {peak}")
-    # print(f"The index of the peak on the right is {peak_2}")
-    
+
+    print(f"The index of the peak on the left is {peak}")
+    print(f"The index of the peak on the right is {peak_2}")
+
 # peaks()
 
-# def valleys():
-#     length = len(data)
-#     middle_index = length//2
-
-#     first_half = data[:middle_index]
-#     second_half = data[middle_index:]
-
-#     print (first_half)
-#     print(second_half)
-    # valley = data.index(min(first_half))
-    # valley_2 = data.index(min(second_half))
-
-    # print(f"The index of the valley on the left is {valley}")
-    # print(f"The index of the valley on the right is {valley_2}")
+def valleys():
+    valleys = []
+    for i in noindex[1:]:
+        if data[i] <= data[i-1] and data[i] <= data[i+1]:
+            valleys.append(i)
+    # return valleys
+    
+    print(f"The indices of the valleys are {valleys}")
 
 # valleys()
 
 
-# def peaks_and_valleys():
-#     peaks()
-#     valleys()
-    # length = len(data)
-    # middle_index = length//2
+def peaks_and_valleys():
+    peaks()
+    valleys()
+    
+peaks_and_valleys()
 
-    # first_half = data[:middle_index]
-    # second_half = data[middle_index:]
-    # # peak = first_half.index('P')
-
-    # peak = data.index(max(first_half))
-    # peak_2 = data.index(max(second_half))
-
-    # print(f"The index of the peak on the left is {peak}")
-    # print(f"The index of the peak on the right is {peak_2}")
-
-
-    # length = len(data)
-    # middle_index = length//2
-
-    # first_half = data[:middle_index]
-    # second_half = data[middle_index:]
-
-    # valley = data.index(min(first_half))
-    # valley_2 = data.index(min(second_half))
-
-    # print(f"The index of the valley on the left is {valley}")
-    # print(f"The index of the valley on the right is {valley_2}")
-
-# peaks_and_valleys()
 
 
 #jon's ex: 
-
+"""
 def get_data():
     data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
     return data
@@ -104,3 +72,4 @@ def get_peaks(data):
             if data[i] >= data [i-1] and data [i] >= data [i+1]:
                 peaks.append(i)
     return peaks 
+"""
