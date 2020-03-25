75856c3f2d50d974cb612c2c02891d9f5263056e
Lynn <lynn@Sheris-MacBook-Air.local>
2020-03-24 19:00:24 -0700

    lab14 update

--- python/labs/lab14.py
+++ python/labs/lab14.py
@@ -12,17 +12,23 @@ def pick6():
             return y
             break
 
+# comp = pick6()
+# yours = pick6()
 
 def num_matches(comp, yours):
 
     match = 0
+
     for i in range(0,6):
         if yours[i] == comp[i]:
             match += 1
         else:
             match += 0 
     return match
-    
+
+
+
+def balance(match):
     balance = 0
     new_balance = balance - 2
     if match == 0: 
@@ -39,21 +45,31 @@ def num_matches(comp, yours):
         new_balance += 1000000
     elif match == 6:
         new_balance += 25000000
-    return balance
+    return new_balance
+
+# print(balance(num_matches(comp,yours)))
+
 
 
 def compare():
+    x = 0 
     current_balance = 0
+    while True:
+        x += 1
+        
+        comp = pick6()
+        yours = pick6()
 
-    comp = pick6()
-    yours = pick6()
+    
+        current_balance = current_balance + balance(num_matches(comp, yours))
 
-    current_balance = current_balance + num_matches(comp, yours)
+        print(f'Winning ticket: {comp}')
+        print(f'Your ticket: {yours}')
+    
 
-    print(f'Winning ticket: {comp}')
-    print(f'Your ticket: {yours}')
-   
+        print(f'Your balance is: ${current_balance}')
 
-    print(f'Your balance is: ${current_balance}')
+        if x == 100000:
+            break
 
 compare()
