9d74d30f12daf48359e7bf9590d7f772d9715a74
Lynn <lynn@Sheris-MacBook-Air.local>
2020-04-01 18:47:22 -0700

    lab23clean

--- python/labs/lab23clean.py
+++ python/labs/lab23clean.py
@@ -2,34 +2,12 @@ import csv
 
 #version 1 
 
-
 phonebook = []
-row_count = 0
-# open csv, set up dictionary
-with open('lab23.csv', 'r') as file:
-    lines = file.read().split('\n')
-    print (lines)
-    # for row in lines:
-
-    #     if row_count == 0:
-    #         row_count+=1
-    #     else: 
-    #         phonebook.append(
-    #             {'name': row[0],
-    #             'favorite fruit': row[1],
-    #             'favorite color': row[2]
-    #             }
-    #         )
-    for row in lines: 
-        if row_count == 0:
-            row_count += 1
-        else:
-            phonebook.append(row[0])
-            phonebook.append(row[1])
-            phonebook.append(row[2])
-            row_count += 1
-
-print(f"Phonebook: {phonebook}")
+with open('lab23.csv') as file:
+    read = csv.DictReader(file, delimiter=',')
+    for row in read:
+        phonebook.append(row)
+    print(phonebook)
 
 #version2
 
@@ -60,13 +38,17 @@ print(f"Phonebook: {phonebook}")
 
 """Retrieve a record: ask the user for the contact's name, find the user with the given name, and display their information"""
 
-# user_input = input("who's name do you want to look up? ")
+phonebook = []
+with open('lab23.csv') as file:
+    read = csv.DictReader(file, delimiter=',')
+    for row in read:
+        phonebook.append(row)
+    print(phonebook)
 
-# with open('lab23.csv', 'r') as phone_book_file:
-#     for line in phone_book_file:
-#         print(line)
-#         # if user_input == line[0]:
-#         #     print(line)
-#         # else:
-#         #     print("cant find")
+user_input = input("Please enter the name of the person you would information of. ").lower()
+
+for row in phonebook:
+    if row['name'] == user_input:
+        print(row)
+   
 
