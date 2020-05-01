c505447b59cb5b018fbb8b423f029034ed472eba
nguyenl1 <lynnthuynguyen@yahoo.com>
2020-04-30 21:17:32 -0700

    fix pages to list tasks. still need to save data.

--- django/lab2/todo_app/urls.py
+++ django/lab2/todo_app/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns=[
     path("", views.todo_app, name = "todo_app"),
-    # path('mytasks', views.showTasks, name="showTasks"),
     path('createtasks', views.create_tasks, name="createTasks"),
-    path('task/<int:pk>/', views.task_detail, name ='tasks_detail'),
-    path('viewtasks', views.list_tasks, name="viewTasks"),
+    path('viewtasks', views.showTasks, name="viewTasks"),
     path('removetasks', views.remove_task, name= "remove"),
 ]
 
--- django/lab2/todo_app/views.py
+++ django/lab2/todo_app/views.py
@@ -2,39 +2,21 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Task 
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 
+
+
 def todo_app(request):
-    return render(request, "index.html", {})
+    tasks = Task.objects.all()
 
-def create_tasks(request):
-    if request.method == 'POST':
-        data = request.POST
-        Task.objects.create(
-            task_name = data['task_name'],
-            desp = data['desp'],
-            due_date = data['due_date']
+    context = {
+        'tasks':tasks,
+    }
 
-        )
-
-        return redirect('todo_app')
-    else:
-        return HttpResponse('invalid method')
-
-
-def task_detail(request,pk):
-    task = get_list_or_404(Task, pk=pk)
-    return render(request,'view_tasks.html', {'post':post})
-
-def remove_task(request):
-    return render(request, "remove_tasks.html")
-
-def list_tasks(request):
-    return render(request, "view_tasks.html")
-
-def completed(request):
-    pass 
+    return render(request, "index.html", context=context)
 
 def showTasks(request):
     tasks = Task.objects.all()
@@ -43,4 +25,29 @@ def showTasks(request):
         'tasks':tasks, 
     }
 
-    return render(request,'index.html', context=context)
+    return render(request,'view_tasks.html', context=context)
+
+def create_tasks(request):
+    # if request.method == 'POST':
+    #     data = request.POST
+    #     Task.objects.create(
+    #         task_name = data['task_name'],
+    #         due_date = data['due_date'],
+    #         is_completed = data['is_completed']
+    #     )
+
+    #     task_name.save()
+    #     due_date.save()
+    #     is_completed()
+
+        return render(request, "create_task.html")
+    # else:
+    #     return HttpResponse('invalid method')
+
+def remove_task(request):
+    return render(request, "remove_tasks.html")
+
+def completed(request):
+    pass 
+
+
