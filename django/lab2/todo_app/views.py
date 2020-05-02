05d8e9b191568aa3ecc8ff06bb18f9b08d744e9b
nguyenl1 <lynnthuynguyen@yahoo.com>
2020-05-01 21:11:36 -0700

    create task function works, but not in the way i want it to

--- django/lab2/todo_app/views.py
+++ django/lab2/todo_app/views.py
@@ -27,21 +27,25 @@ def show_tasks(request):
 
     return render(request,'view_tasks.html', context=context)
 
-def create_tasks(request):
-    if request.method == 'POST':
-        data = Task(request.POST)
+# def create_tasks(request):
+#     if request.method == 'POST':
+#         if request.POST.get('task_name') and request.POST.get('due_date') and request.POST.get('is_completed'):
         
-        is_completed = False
+        
 
-        create_task = Task()
-        create_task.task_name = task_name
-        create_task.due_date = due_date 
-        create_task.is_completed = is_completed
-        create_task.save()
+#             create_task = Task()
+#             create_task.task_name = request.POST.get('task_name')
+#             create_task.due_date = request.POST.get('due_date')
+#             create_task.is_completed = request.POST.get('is_completed')
+#             create_task.save()
 
-        return render(request, "create_task.html")
-    else:
-        return HttpResponse('invalid method')
+#             return render(request, "create_task.html")
+#     else:
+#         return HttpResponse('invalid method')
+
+def create_tasks(request):
+    return render(request, "create_task.html")
+
 
 def remove_tasks(request,id):
     return render(request, "remove_tasks.html")
@@ -51,8 +55,6 @@ def completed(request):
 
 def show_details(request,id):
     desp = Task.objects.get(pk=id)
-
-    print(desp.task_name)
 
     context = {
         'desp':desp,
