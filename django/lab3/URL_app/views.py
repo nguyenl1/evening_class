a3f7582d11ebad79bc2a29b8a51fc3ab56020406
nguyenl1 <lynnthuynguyen@yahoo.com>
2020-05-07 19:28:56 -0700

    got the short_url to print

--- django/lab3/URL_app/views.py
+++ django/lab3/URL_app/views.py
@@ -13,27 +13,25 @@ def home(request):
 
         UrlForm.objects.create(site_name=site_name, url_link=url_link, )
 
+        last = UrlForm.objects.last()
+        short = last.url_link
+        new_url = last.short_url()
+
+        print (new_url)
     return render(request, "home.html")
+
+def convert(request):
+
+    last = UrlForm.objects.last()
+    short = last.short_url()
+    short = last.url_link
+
+    print (short)
+
 
 def save_url(request):
     return redirect("home")
 
-def code(request):
-    new = string.ascii_letters + string.digits
-
-    i = ""
-    y = 0
-    while 0 <= 6:
-        y = y + 1
-        i += random.choice(new)
-        if y == 6:
-            break
-
-    new_url = i 
-
-    print("https://www.myurl.com/" + f"{new_url}")
-
-    pass 
 
 def view_url(request):
     list_urls = UrlForm.objects.all()
@@ -43,4 +41,7 @@ def view_url(request):
     
     }
     return render(request,"view_urls.html", context=context)
+
+def new_url(request,id):
+    pass 
 
