7fcaee4bc91fd7ca0b408a4066a0bfbd82bb574f
nguyenl1 <lynnthuynguyen@yahoo.com>
2020-05-14 18:54:24 -0700

    set up the base of the app, need to add models in

---
+++ django/lab4/accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth import get_user_model, login, logout, authenticate
+
+def register_user(request):
+    pass
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
