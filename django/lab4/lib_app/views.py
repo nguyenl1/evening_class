5cfedb3b01cd3ebd75aa1c6ace4801186b387e60
nguyenl1 <lynnthuynguyen@yahoo.com>
2020-05-22 18:53:09 -0700

    got the status of the books to change and books are displayed on the my books list

--- django/lab4/lib_app/views.py
+++ django/lab4/lib_app/views.py
@@ -26,29 +26,40 @@ def book_details (request,id):
     return render(request, "lib_app/book_details.html", context=context)
 
 def checked_out (request,id):
-        if request.method == 'POST':
+    book = Book.objects.get(pk=id)
+    if request.method == 'POST':
             
-            check = Checkout(request.POST)
-            book = check.book = book
-            borrower = check.borrower = request.user
-            checkout_date = check.checkout_date = timezone.now()
-            due_date = check.due()
+        check = Checkout(request.POST)
+        book = check.book = book
+        borrower = check.borrower = request.user
+        checkout_date = check.checkout_date = timezone.now()
+        due_date = check.due()
+        status = 'u'
 
-            Checkout.objects.create(book=book, borrower=borrower, checkout_date=checkout_date, due_date=due_date,)
+        Checkout.objects.create(book=book, borrower=borrower, checkout_date=checkout_date, due_date=due_date, status=status,)
 
-            return redirect('list_books')
-        return render(request,"lib_app/list_books.html")
+        return redirect('my_books')
+    return render(request,"lib_app/list_books.html")
 
 def my_books (request):
-    books = Checkout.objects.filter(borrower=request.user)
-
+    books = Checkout.objects.filter(borrower=request.user, status='u')
     context = {
         'books':books,
     }
     
     return render(request,'lib_app/my_books.html', context)
+    
 
-def return_book(request):
-    avail = get_object_or_404(Checkout, pk=id)
-    avail.delete()
-    return redirect('home')
+def return_book(request,id):
+    avail = Checkout.objects.get(pk=id)
+    avail.status = 'a'
+    avail.save()
+    # if request.POST.get('status') == 'u':
+        
+
+    return redirect('my_books')
+
+
+
+    
+
