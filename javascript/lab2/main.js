34d31da4c96f1c0c1d3cc92587a58ff5d8700805
nguyenl1 <lynnthuynguyen@yahoo.com>
2020-06-03 21:23:33 -0700

    add task and list task

--- javascript/lab2/main.js
+++ javascript/lab2/main.js
@@ -1,18 +1,37 @@
-const hello = document.getElementById("demo");
-hello.innerHTML="Hello World!"
 
-hello.addEventListener('click', function() {
-    alert('Hello World!');
-    // const task = document.getElementById('taskname').value;
+// hello.addEventListener('click', function() {
+//     alert('Hello World!');
+//     // const task = document.getElementById('taskname').value;
 
-    // console.log(task)
-});
+//     // console.log(task)
+// });
 
-const task = document.getElementById('taskname').value;
-
-console.log(task)
-
-// var todo = document.getElementsByTagName("ul"); 
 
 let mytasks = [] 
 
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    console.log("Saving value", form.taskname.value);
+
+    event.preventDefault();
+
+    mytasks.push(form.taskname.value);
+
+    let ul = document.getElementById("listtasks").innerHTML=mytasks;
+    document.forms[0].reset();
+    
+});
+
+
+function removetask() {
+
+}
+
+function complete() {
+
+}
+
+function main() {
+
+}
