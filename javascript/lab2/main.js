e88c70e0619b81c1d95197a97cfaaa924f22c4a8
nguyenl1 <lynnthuynguyen@yahoo.com>
2020-06-04 21:19:52 -0700

    working on completing a task

--- javascript/lab2/main.js
+++ javascript/lab2/main.js
@@ -11,27 +11,47 @@ let mytasks = []
 
 let form = document.querySelector("form");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function(addtask) {
     console.log("Saving value", form.taskname.value);
 
-    event.preventDefault();
+    addtask.preventDefault();
 
     mytasks.push(form.taskname.value);
 
-    let ul = document.getElementById("listtasks").innerHTML=mytasks;
-    document.forms[0].reset();
+    console.log(mytasks)
+
+    // let checkbox = document.createElement('input');
+    // checkbox.type = "checkbox";
+    // checkbox.name = "name";
+    // checkbox.value = "value";
+    // checkbox.id = "id";
+
+    let tasklen = mytasks.length;
+
+    let list = "<ul>";
+
+    for (i = 0; i < tasklen; i++) {
+        list += "<li>" + mytasks[i] + "</li>";
+        
+    }
+
+    list += "</ul>";
+
+    document.getElementById("listtasks").innerHTML = list; 
+
     
+
+    document.forms[0].reset();
+
 });
-
-
-function removetask() {
-
-}
 
 function complete() {
 
 }
 
-function main() {
+function removetask() {
 
 }
+
+
+
